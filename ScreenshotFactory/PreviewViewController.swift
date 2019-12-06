2b8a7409e84f73eee8b5b02122f2f85637271662
Filip Němeček <filip.nemecek@pm.me>
2019-12-06 14:58:22 +0100

    Cleanup

--- ScreenshotFactory/PreviewViewController.swift
+++ ScreenshotFactory/PreviewViewController.swift
@@ -15,8 +15,6 @@ class PreviewViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
         
         previewImageView.image = image
     }
@@ -28,16 +26,4 @@ class PreviewViewController: UIViewController {
     @IBAction func closeButtonTapped(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
-    
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
